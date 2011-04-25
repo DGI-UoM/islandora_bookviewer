e18efe0dba5f5852aa18dc5d7c4eddc16a79c399
ppound <paul.pound@gmail.com>
2011-04-25 16:38:59 -0300

    updated print function

--- plugins/BookReader/BookReader.js
+++ plugins/BookReader/BookReader.js
@@ -3231,7 +3231,7 @@ BookReader.prototype.removeSearchHilites = function() {
 // printPage
 //______________________________________________________________________________
 BookReader.prototype.printPage = function() {
-    window.open(this.getPrintURI(), 'printpage', 'width=400, height=500, resizable=yes, scrollbars=no, toolbar=no, location=no');
+    window.open(this.getPrintURI(), 'printpage', 'width=400, height=500, resizable=yes, scrollbars=yes, toolbar=yes, location=no');
 }
 
 // Get print URI from current indices and mode
@@ -3242,20 +3242,21 @@ BookReader.prototype.getPrintURI = function() {
     } else {
         indexToPrint = this.firstIndex; // $$$ the index in the middle of the viewport would make more sense
     }
-    
-    var options = 'id=' + this.subPrefix + '&server=' + this.server + '&zip=' + this.zip
-        + '&format=' + this.imageFormat + '&file=' + this._getPageFile(indexToPrint)
-        + '&width=' + this._getPageWidth(indexToPrint) + '&height=' + this._getPageHeight(indexToPrint);
-   
-    if (this.constMode2up == this.mode || this.constModeImageAndText == this.mode) {
-        options += '&file2=' + this._getPageFile(this.twoPage.currentIndexR) + '&width2=' + this._getPageWidth(this.twoPage.currentIndexR);
-        options += '&height2=' + this._getPageHeight(this.twoPage.currentIndexR);
-        options += '&title=' + encodeURIComponent(this.shortTitle(50) + ' - Pages ' + this.getPageNum(this.twoPage.currentIndexL) + ', ' + this.getPageNum(this.twoPage.currentIndexR));
-    } else {
-        options += '&title=' + encodeURIComponent(this.shortTitle(50) + ' - Page ' + this.getPageNum(indexToPrint));
-    }
 
-    return '/bookreader/print.php?' + options;
+    var pid = this.getPid(indexToPrint);
+   // var options = 'id=' + this.subPrefix + '&server=' + this.server + '&zip=' + this.zip
+   //     + '&format=' + this.imageFormat + '&file=' + this._getPageFile(indexToPrint)
+   //     + '&width=' + this._getPageWidth(indexToPrint) + '&height=' + this._getPageHeight(indexToPrint);
+   
+    //if (this.constMode2up == this.mode || this.constModeImageAndText == this.mode) {
+   //     options += '&file2=' + this._getPageFile(this.twoPage.currentIndexR) + '&width2=' + this._getPageWidth(this.twoPage.currentIndexR);
+    //    options += '&height2=' + this._getPageHeight(this.twoPage.currentIndexR);
+    //    options += '&title=' + encodeURIComponent(this.shortTitle(50) + ' - Pages ' + this.getPageNum(this.twoPage.currentIndexL) + ', ' + this.getPageNum(this.twoPage.currentIndexR));
+    //} else {
+    //    options += '&title=' + encodeURIComponent(this.shortTitle(50) + ' - Page ' + this.getPageNum(indexToPrint));
+   // }
+
+    return islandora_prefix+pid+'/PDF';
 }
 
 /* iframe implementation
